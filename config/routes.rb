44e89667c5f6248e685ee4451c9520797ac4ced9
themaxta <themaxta@gmail.com>
2016-02-15 22:52:23 -0700

    Link to menu works. need menu picture.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  
-  get "/pages/:page" => "pages#show"
+  get "/pages/menu"
+
+
+   #-> sends params[:id] as /page_name
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
